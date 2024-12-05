3b0e8a450f0264c5ba0d89c5e8936559c4c80749
FacuGerez <facundogerezuba@gmail.com>
2024-12-05 11:21:52 -0300

    addjust new paths and add method post and put

---
+++ Front/src/components/Forms.jsx
@@ -0,0 +1,76 @@
+import { useForm } from 'react-hook-form';
+import { useState, useEffect } from 'react';
+import links from '../../public/links.json';
+
+export const FormToRole = ({ url, method }) => {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error('Error:', error));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register('name')} required/>
+      <input type="text" {...register('description')} required/>
+      <button type="submit">Enviar</button>
+    </form>
+  );
+};
+
+export const FormToUser = ({ url, method }) => {
+  const { register, handleSubmit } = useForm();
+  const [items, setItems] = useState([]);
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(links['development-backend']['other-links'].sql.roles['get-roles']);
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error('Error al obtener los datos:', error);
+      }
+    };
+
+    fetchItems();
+  }, []); // Se ejecuta solo una vez
+
+  const onSubmit = (data) => {
+    fetch(url, {
+      method: method,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error('Error:', error));
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input type="text" {...register('name')} required/>
+      <input type="text" {...register('email')} required/>
+      <input list="roles-list" required/>
+      <datalist id="roles-list">
+        {items.length === 0 ?
+          <option value="No hay roles creados todavia">No hay roles creados todavia</option> :
+           items.map((item) => (
+            <option key={item.id} value={item.name}>{item.name}</option>
+          ))
+        }
+      </datalist>
+      <button type="submit">Enviar</button>
+    </form>
+  );
+};
